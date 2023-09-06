8539da4f1fa30fe5bfeb7ddf610a980f25c42953
Dijar06 <dize06001@utb.vaxjo.se>
2023-09-06 10:15:00 +0200

    Add project files.

---
+++ Uppgift1.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Uppgift1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1."); // Skriver texten på console
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\"); //Skriver texten på console med " och \
+        }
+    }
+}
